d4755d690989242e81b1da2863072ce831012d68
guojian <guojian.pan@ly.com>
2020-07-31 17:27:22 +0800

    配置文件添加

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "src/main.js",
+  output: {
+    name: "index",
+    file: "dist/index.js",
+    format: "umd",
+    exports: "default",
+    compact: true, // build 后代码紧凑
+    noConflict: true,
+    sourcemap: true,
+    sourcemapExcludeSources: true,
+    minifyInternalExports: true,
+  },
+  external: [],
+  treeshake: true,
+  plugins: [resolve(), commonjs(), json(), babel()],
+};
